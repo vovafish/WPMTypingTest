a06ca71f1c94a7c6baedb8a4697a96af4f9841f2
Vladimirs <vladimirribakov123@mail.ru>
2024-06-13 22:29:26 +0100

    added a start screen for the user that asks to press any key to start the game

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import curses
 from curses import wrapper
 
 
+def start_screen(stdsrc):
+    stdsrc.clear()
+    stdsrc.addstr("Welcome to the Speed Typing Test!")
+    stdsrc.addstr("\nPress any key to begin!")
+    stdsrc.refresh()
+    stdsrc.getkey()
+
+
 def main(stdsrc):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_WHITE)
 
-    stdsrc.clear()
-    stdsrc.addstr(0, 0, "Hi World!")
-    stdsrc.refresh()
-    key = stdsrc.getkey()
-    print(key)
+    start_screen(stdsrc)
 
 
 wrapper(main)
