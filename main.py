7c588e140a70f1adc7694aa3d42387e7c1220a0f
Vladimirs <vladimirribakov123@mail.ru>
2024-06-14 19:30:30 +0100

    created test text that pops up after user starts the game

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ import curses
 from curses import wrapper
 
 
-def start_screen(stdsrc):
-    stdsrc.clear()
-    stdsrc.addstr("Welcome to the Speed Typing Test!")
-    stdsrc.addstr("\nPress any key to begin!")
-    stdsrc.refresh()
-    stdsrc.getkey()
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the Speed Typing Test!")
+    stdscr.addstr("\nPress any key to begin!")
+    stdscr.refresh()
+    stdscr.getkey()
 
 
-def main(stdsrc):
+def wpm_test(stdscr):
+    target_text = "Hello wold this is some sort of text for this app!"
+    current_text = []
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_WHITE)
 
-    start_screen(stdsrc)
+    start_screen(stdscr)
+    wpm_test(stdscr)
 
 
 wrapper(main)
