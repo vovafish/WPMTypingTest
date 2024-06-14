140e0d06e462b4c20c9779e4419c59c24ed333d0
Vladimirs <vladimirribakov123@mail.ru>
2024-06-14 20:17:00 +0100

    putting typed text on top of existing text and if text if correct then make the text green otherwise red

--- main.py
+++ main.py
@@ -10,17 +10,26 @@ def start_screen(stdscr):
     stdscr.getkey()
 
 
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
+
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+        stdscr.addstr(0, i, char, color)
+
+
 def wpm_test(stdscr):
     target_text = "Hello wold this is some sort of text for this app!"
     current_text = []
+    wpm = 0
 
     while True:
         stdscr.clear()
-        stdscr.addstr(target_text)
-
-        for char in current_text:
-            stdscr.addstr(char, curses.color_pair(1))
-
+        display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
 
         key = stdscr.getkey()
@@ -31,7 +40,7 @@ def wpm_test(stdscr):
         if key in ("KEY_BACKSPACE", '\b', "\x7f"):
             if len(current_text) > 0:
                 current_text.pop()
-        else:
+        elif len(current_text) < len(target_text):
             current_text.append(key)
 
 
