872b09073d7a0a4ef1c28b60d394298b1b0394f3
Vladimirs <vladimirribakov123@mail.ru>
2024-06-14 20:01:44 +0100

    if presses 'esc' it exists the app. all the text that got typed can be removed by pressing backspace.

--- main.py
+++ main.py
@@ -13,16 +13,32 @@ def start_screen(stdscr):
 def wpm_test(stdscr):
     target_text = "Hello wold this is some sort of text for this app!"
     current_text = []
-    stdscr.clear()
-    stdscr.addstr(target_text)
-    stdscr.refresh()
-    stdscr.getkey()
+
+    while True:
+        stdscr.clear()
+        stdscr.addstr(target_text)
+
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+
+        stdscr.refresh()
+
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
 
 
 def main(stdscr):
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
-    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_WHITE)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
     wpm_test(stdscr)
